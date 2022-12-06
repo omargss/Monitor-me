65188d67bf7ec482172a5368dd36e10faab08a84
BIER <cyril.bier42100@gmail.com>
2022-12-06 15:00:13 +0100

    Ajout de fonction getAccesLog

--- script/readlog.py
+++ script/readlog.py
@@ -1,18 +1,30 @@
 from connection import client    
-# First, we get the memory informations and stock it in an array with, in order: Total, used, free, shared, buff/cache, available
 _, stdout, stderr = client.exec_command('cat /var/log/apache2/other_vhosts_access.log | grep "GET /?"')
 output = stdout.read().decode("utf-8")
 
 class WebPage():
+    def __init__(self, Name, Number):
+        self.name = Name
+        self.numberOfSearchs = Number
     name=""
     numberOfSearchs=0
-    
-consultatedPages = []
 
-line1 = output.splitlines()[0].split()
+def getAccessLogs():
+    consultatedWebPages = []
+    for line in output.splitlines():
+        trouve = 0
+        for wp in consultatedWebPages:
+            if wp.name == line.split()[7]:
+                wp.numberOfSearchs += 1
+                trouve =1
+        if trouve == 0:
+            newWp = WebPage(line.split()[7],1)
+            consultatedWebPages.append(newWp)
+    return consultatedWebPages
 
-for line in output.splitlines():
-    if not(line.split()[7] in consultatedPages):
-        consultatedPages.append(line.split()[7])
 
-print(consultatedPages)
+
+consultatedPages = getAccessLogs()
+
+for wp in consultatedPages:
+    print(wp.name + " "+str(wp.numberOfSearchs))
