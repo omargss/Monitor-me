04b87e3216a796ebda0eddb47d0321b5ceb35dc2
BIER <cyril.bier42100@gmail.com>
2022-12-06 17:55:26 +0100

    Ajout d'une méthode getErrors

--- script/readlog.py
+++ script/readlog.py
@@ -26,7 +26,58 @@ def get_access_logs():
             consultated_web_pages.append(new_web_page)
     return consultated_web_pages
 
-consultatedPages = get_access_logs()
+def get_error_logs():
+    """Retourne les logs d'erreur"""
+    cmd = 'cat /var/log/apache2/error.log | grep "warn"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    warn_list = []
+    for line in output.splitlines():
+        warn_list.append(line)
+    
+    cmd = 'cat /var/log/apache2/error.log | grep "emerg"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    emerg_list = []
+    for line in output.splitlines():
+        emerg_list.append(line)
 
-for wp in consultatedPages:
-    print(wp.name + " "+str(wp.numberOfSearchs))
+    cmd = 'cat /var/log/apache2/error.log | grep "alert"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    alert_list = []
+    for line in output.splitlines():
+        alert_list.append(line)
+
+    cmd = 'cat /var/log/apache2/error.log | grep "crit"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    crit_list = []
+    for line in output.splitlines():
+        crit_list.append(line)
+
+    cmd = 'cat /var/log/apache2/error.log | grep "error"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    error_list = []
+    for line in output.splitlines():
+        error_list.append(line)
+
+    cmd = 'cat /var/log/apache2/error.log | grep "notice"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    notice_list = []
+    for line in output.splitlines():
+        notice_list.append(line)
+
+    cmd = 'cat /var/log/apache2/error.log | grep "debug"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    debug_list = []
+    for line in output.splitlines():
+        debug_list.append(line)
+
+    return warn_list, emerg_list,alert_list, crit_list, error_list, notice_list,debug_list
+
+warnlist = get_error_logs()
+print(len(warnlist))
