4a48919a6f586b332dbed85836633064110c7a3b
Mathis <mathis.thuel@gmail.com>
2022-12-13 15:08:28 +0100

    new web displays

--- script/web_app/app.py
+++ script/web_app/app.py
@@ -3,9 +3,9 @@ import configparser
 import paramiko
 from dash import Dash, html, dcc
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import functions
-import plotly.graph_objects as go
 
 app = Dash(__name__)
 
@@ -23,7 +23,6 @@ client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 client.connect(hostname = hostname, port=port, username=username, password=password)
-
 """ MEMORY CHART """
 memorylist = functions.get_memory(client)
 memorydata = {
@@ -34,22 +33,28 @@ df = pd.DataFrame(memorydata)
 memoryGraph = px.bar(df, x="Memory_Type", y="Memory",
 color="Memory_Type", title="Usage of the memory of the remote server", barmode='stack')
 memoryGraph.update_layout(xaxis_title='Name')
+""" PROCESSES CHART """
+processlist = functions.get_process_infos(client)
+process_labels = []
+process_values = []
+for process in processlist:
+    process_labels.append(process.command)
+    process_values.append(1) #process.cpu
+processPieChart = go.Figure(data=[go.Pie(labels=process_labels, values=process_values,
+title="Pie chart representing cpu usage by processes")])
 """ LOGS CHART """
-logs = functions.getAccessLogs(client)
+logs = functions.get_access_logs(client)
 memory_labels = []
 memory_values = []
 for log in logs:
     memory_labels.append(log.name)
     memory_values.append(log.numberOfSearchs)
-logPieChart = go.Figure(data=[go.Pie(labels=memory_labels, values=memory_values)])
-""" PROCESSES CHART """
-processlist = functions.getProcessInfos(client)
-process_labels = []
-process_values = []
-for process in processlist:
-    process_labels.append(process.command)
-    process_values.append(10)
-processPieChart = go.Figure(data=[go.Pie(labels=process_labels, values=process_values)])
+logPieChart = go.Figure(data=[go.Pie(labels=memory_labels, values=memory_values,
+title="Pie chart representing logs access")])
+
+list1= []
+list2 = []
+list1,list2 = functions.get_biggest_files(client)
 """ WEB DISPLAY """
 app.layout = html.Div(children= [
     html.H1(children='Monitoring web application'),
@@ -59,15 +64,15 @@ app.layout = html.Div(children= [
         id='Memory Graph',
         figure=memoryGraph
     ),
-    html.H2(children='Logs'),
-    dcc.Graph(
-        id='Pie Logs chart',
-        figure=logPieChart
-    ),
     html.H2(children='Processes'),
     dcc.Graph(
         id='Pie process chart',
         figure=processPieChart
+    ),
+    html.H2(children='Logs'),
+    dcc.Graph(
+        id='Pie Logs chart',
+        figure=logPieChart
     ),
 ])
 
--- script/web_app/functions.py
+++ script/web_app/functions.py
@@ -1,8 +1,5 @@
-import paramiko;
-import sys
-import configparser
-
-class Process():
+# Class Process used to store processes
+class Process:
     def __init__(self, u, p, c, m, s, co):
         self.user = u
         self.cpu = c
@@ -10,6 +7,7 @@ class Process():
         self.memory = m
         self.start_time = s
         self.command = co
+
     user = ""
     cpu = 0
     pid = 0
@@ -17,43 +15,79 @@ class Process():
     start_time = ""
     command = ""
 
-class WebPage():
-    def __init__(self, Name, Number):
-        self.name = Name
-        self.numberOfSearchs = Number
-    name=""
-    numberOfSearchs=0
+
+# Class WebPage used to store web pages
+""" Class WebPage """
+
+
+class WebPage:
+    def __init__(self, name, number):
+        self.name = name
+        self.numberOfSearchs = number
+
+    name = ""
+    numberOfSearchs = 0
+
 
 def get_memory(client):
-  _, stdout, stderr = client.exec_command("free | grep -n Mem -")
-  output = stdout.read().decode("utf-8")
-  memorylist = output.splitlines()[0].split()
-  del memorylist[0]
-  for i in range(len(memorylist)):
-    memorylist[i] = int(memorylist[i])
-  return memorylist
+    _, stdout, stderr = client.exec_command("free | grep -n Mem -")
+    output = stdout.read().decode("utf-8")
+    memorylist = output.splitlines()[0].split()
+    del memorylist[0]
+    for i in range(len(memorylist)):
+        memorylist[i] = int(memorylist[i])
+    return memorylist
 
-def getAccessLogs(client):
-	consultatedWebPages = []
-	_, stdout, stderr = client.exec_command('cat /var/log/apache2/other_vhosts_access.log | grep "GET /?"')
-	output = stdout.read().decode("utf-8")
-	for line in output.splitlines():
-		trouve = 0
-		for wp in consultatedWebPages:
-			if wp.name == line.split()[7]:
-				wp.numberOfSearchs += 1
-				trouve =1
-		if trouve == 0:
-			newWp = WebPage(line.split()[7],1)
-			consultatedWebPages.append(newWp)
-	return consultatedWebPages
 
-def getProcessInfos(client):
-	list_process = []
-	_, stdout, stderr = client.exec_command("ps -aux | sort -k 3 -r | grep -wv USER | head -n 10")
-	output = stdout.read().decode("utf-8")
-	for line in output.splitlines():
-		line = line.split()
-		process = Process(line[0],line[1],line[2],line[3],line[8] +"-"+ line[9], (" ").join(line[10:len(line)]))
-		list_process.append(process)
-	return list_process
+def get_access_logs(client):
+    consultatedWebPages = []
+    _, stdout, stderr = client.exec_command(
+        'cat /var/log/apache2/other_vhosts_access.log | grep "GET /?"'
+    )
+    output = stdout.read().decode("utf-8")
+    for line in output.splitlines():
+        trouve = 0
+        for wp in consultatedWebPages:
+            if wp.name == line.split()[7]:
+                wp.numberOfSearchs += 1
+                trouve = 1
+        if trouve == 0:
+            newWp = WebPage(line.split()[7], 1)
+            consultatedWebPages.append(newWp)
+    return consultatedWebPages
+
+
+def get_process_infos(client):
+    list_process = []
+    _, stdout, stderr = client.exec_command(
+        "ps -aux | sort -k 3 -r | grep -wv USER | head -n 10"
+    )
+    output = stdout.read().decode("utf-8")
+    for line in output.splitlines():
+        line = line.split()
+        process = Process(
+            line[0],
+            line[1],
+            line[2],
+            line[3],
+            line[8] + "-" + line[9],
+            (" ").join(line[10 : len(line)]),
+        )
+        list_process.append(process)
+    return list_process
+
+
+def get_biggest_files(client):
+    list_files = []
+    list_size = []
+    _, stdout, stderr = client.exec_command(
+        "du -ah 2>/dev/null | sort -n -r | head -n 10"
+    )
+    output = stdout.read().decode("utf-8")
+    for line in output.splitlines():
+        line = line.split()
+        print(line[0])
+        print(line[1])
+        list_files.append(line[0])
+        list_size.append(line[1])
+    return list_files, list_size
