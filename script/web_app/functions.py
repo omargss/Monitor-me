fecc757d0efa60e9b5665a3daf18f66bcfd58fff
Mathis <mathis.thuel@gmail.com>
2023-01-03 10:09:54 +0100

    modifying functions.py

--- script/web_app/functions.py
+++ script/web_app/functions.py
@@ -43,25 +43,80 @@ def get_memory(client):
     return memorylist
 
 def AnalyzeLogs(file):
-  consultatedWebPages = []
-  for line in file.splitlines():
-    trouve = 0
-    for wp in consultatedWebPages:
-      if wp.name == line.split()[7]:
-        wp.numberOfSearchs += 1
-        trouve = 1
-    if trouve == 0:
-      newWp = WebPage(line.split()[7], 1)
-      consultatedWebPages.append(newWp)
-  return consultatedWebPages
+    """ Analyse logs function """
+    consultatedWebPages = []
+    for line in file.splitlines():
+        trouve = 0
+        for wp in consultatedWebPages:
+            if wp.name == line.split()[7]:
+                wp.numberOfSearchs += 1
+            trouve = 1
+        if trouve == 0:
+            newWp = WebPage(line.split()[7], 1)
+            consultatedWebPages.append(newWp)
+    return consultatedWebPages
+
 
 def get_access_logs(client):
-    _, stdout, stderr = client.exec_command(
+    """ get access function """
+    _, stdout, _ = client.exec_command(
         'cat /var/log/apache2/other_vhosts_access.log | grep "GET /?"'
     )
     output = stdout.read().decode("utf-8")
     ConsultatedWebPages = AnalyzeLogs(output)
     return ConsultatedWebPages
+
+def get_error_logs(client):
+    """Retourne les logs d'erreur"""
+    cmd = 'cat /var/log/apache2/error.log | grep "warn"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    warn_list = []
+    for line in output.splitlines():
+        warn_list.append(line)
+    cmd = 'cat /var/log/apache2/error.log | grep "emerg"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    emerg_list = []
+    for line in output.splitlines():
+        emerg_list.append(line)
+
+    cmd = 'cat /var/log/apache2/error.log | grep "alert"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    alert_list = []
+    for line in output.splitlines():
+        alert_list.append(line)
+
+    cmd = 'cat /var/log/apache2/error.log | grep "crit"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    crit_list = []
+    for line in output.splitlines():
+        crit_list.append(line)
+
+    cmd = 'cat /var/log/apache2/error.log | grep "error"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    error_list = []
+    for line in output.splitlines():
+        error_list.append(line)
+
+    cmd = 'cat /var/log/apache2/error.log | grep "notice"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    notice_list = []
+    for line in output.splitlines():
+        notice_list.append(line)
+
+    cmd = 'cat /var/log/apache2/error.log | grep "debug"'
+    _, stdout, _ = client.exec_command(cmd)
+    output = stdout.read().decode("utf-8")
+    debug_list = []
+    for line in output.splitlines():
+        debug_list.append(line)
+
+    return warn_list, emerg_list,alert_list, crit_list, error_list, notice_list,debug_list
 
 
 def get_process_infos(client):
