a3a3d8a75004f2ff29525c930fef5d153e7968b0
Mathis <mathis.thuel@gmail.com>
2023-01-11 15:42:25 +0100

    adding css and details

--- script/web_app/app.py
+++ script/web_app/app.py
@@ -31,20 +31,25 @@ if __name__ == '__main__':
     client.connect(hostname=hostname, port=port,
                    username=username, password=password)
 """ WEB DISPLAY """
+COLOR1 ='#272643'
+COLOR2 ='#2c698d'
+COLOR3 ='#ffffff'
 app.layout = html.Div(children=[
     dcc.Location(id='url', refresh=False),
     html.H1(children='Monitoring web application'),
     html.Div([
-        html.Label('Hostname'),
+        html.Label('Hostname: '),
         dcc.Input(id='hostname', value=hostname, type='text'),
-        html.Label('port'),
+        html.Label('Port: '),
         dcc.Input(id='port', value=port, type='text'),
-        html.Label('username'),
+        html.Label('Username: '),
         dcc.Input(id='username', value=username, type='text'),
-        html.Label('password'),
+        html.Label('Password: '),
         dcc.Input(id='password', value=password, type='password'),
         html.Button('Connection', id='btnConnection'),
     ]),
+    html.Br(),
+    html.Br(),
     dcc.Loading(id="ls-loading-1",children=[html.Div(id="ls-loading-output-1")], type="default"),
     html.Div(id='Display', children=[
         html.H2(children='Memory'),
@@ -104,10 +109,10 @@ app.layout = html.Div(children=[
 """ CALLBACKS """
 
 @app.callback(Output("ls-loading-output-1",'children'), [Input('url', 'pathname')])
-def loading(number):
+def loading(_):
     """ LOADING STATE"""
     time.sleep(20)
-    return number
+    return ''
 
 
 @app.callback(Output('Memory_pie_chart', 'figure'),
@@ -123,7 +128,7 @@ def update_graph_live_memory(_):
     dataframe = pd.DataFrame(memorydata)
     memory_graph = px.bar(dataframe, x="Memory_Type", y="Memory",
                           color="Memory_Type", title="Usage of the memory of the remote server", barmode='stack')
-    memory_graph.update_layout(xaxis_title='Error type')
+    memory_graph.update_layout(xaxis_title='Error type', plot_bgcolor=COLOR1,paper_bgcolor=COLOR2,font={"color":COLOR3})
     return memory_graph
 
 
@@ -136,7 +141,7 @@ def update_graph_live_file_size(_):
     list_size = []
     list_files, list_size = functions.get_biggest_files(client)
     sizedata = go.Bar(x=list_size, y=list_files)
-    return go.Figure(data=sizedata)
+    return go.Figure(data=sizedata).update_layout(plot_bgcolor=COLOR1,paper_bgcolor=COLOR2,font={"color":COLOR3})
 
 
 @app.callback(Output('Pie_process_chart', 'figure'),
@@ -151,7 +156,7 @@ def update_graph_live_process(_):
         process_labels.append(process.command)
         process_values.append(1)  # process.cpu
     return go.Figure(data=[go.Pie(labels=process_labels, values=process_values,
-                                  title="Pie chart representing cpu usage by processes")])
+                                  title="Pie chart representing cpu usage by processes")]).update_layout(plot_bgcolor=COLOR1,paper_bgcolor=COLOR2,font={"color":COLOR3})
 
 
 @app.callback(Output('Logs_graph', 'figure'),
@@ -166,7 +171,7 @@ def update_graph_live_logs(_):
         memory_labels.append(log.name)
         memory_values.append(log.numberOfSearchs)
     return go.Figure(data=[go.Pie(labels=memory_labels, values=memory_values,
-                                  title="Pie chart representing logs access")])
+        title="Pie chart representing most viewed pages")]).update_layout(plot_bgcolor=COLOR1,paper_bgcolor=COLOR2,font={"color":COLOR3})
 
 
 @app.callback(Output('Error_logs_graph', 'figure'),
@@ -182,8 +187,8 @@ def update_graph_live_error_logs(_):
     }
     dataframe = pd.DataFrame(error_data)
     error_graph = px.bar(dataframe, x="Error_Type", y="Errors",
-                         color="Error_Type", title="Log errors", barmode='stack')
-    error_graph.update_layout(xaxis_title='Name')
+                         color="Error_Type", title="Log errors (click on error type to see details)", barmode='stack')
+    error_graph.update_layout(xaxis_title='Error type', plot_bgcolor=COLOR1,paper_bgcolor=COLOR2,font={"color":COLOR3})
     return error_graph
 
 
