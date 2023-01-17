55f6b754df9179969ea31b1bcfa7e785fbfceb3b
THONG <thongcedric@gmail.com>
2023-01-17 19:17:57 +0000

    Update app.py for lint

--- script/web_app/app.py
+++ script/web_app/app.py
@@ -1,3 +1,4 @@
+"""Module dash"""
 from dash import Dash, html
 import dash
 
@@ -9,4 +10,4 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
-	app.run_server(debug=False)
+    app.run_server(debug=False)
